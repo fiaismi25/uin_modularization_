108d620ff41d56a69a3a6015854ef7b83847a4e2
fiaismi25 <56153784+fiaismi25@users.noreply.github.com>
2019-10-18 10:28:04 +0700

    kecepatan tanpa fungsi

---
+++ main.py
@@ -0,0 +1,18 @@
+nama = 'fiaisminura'
+program = 'Gerak Lurus'
+
+print(f'Program {program} oleh {nama}')
+
+jarak = 1000
+waktu = 5 * 60
+kecepatan = jarak / waktu
+
+print(f'Jarak = {jarak/1000} ditempuh dalam waktu = {waktu/60}menit')
+print(f'Sehingga kecepatan = {kecepatan} m/s')
+
+jarak = 5000
+waktu = 10 * 60
+kecepatan = jarak / waktu
+
+print(f'Jarak = {jarak/1000}km ditempuh dalam waktu = {waktu/60}menit')
+print(f'Sehingga kecepatan = {kecepatan}m/s')
